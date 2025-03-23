4d51fead5aa09dffaa4e22b355fc5227534e095e
User <artemenkoe2711@gmail.com>
2025-03-23 18:50:09 +0400

    логику нажатия на кнопку перенес в MainFragmentViewModel. Настроен textWatcher в MainFragment

--- app/src/main/java/com/example/mykeys/main/presentation/viewmodel/MainFragmentViewModel.kt
+++ app/src/main/java/com/example/mykeys/main/presentation/viewmodel/MainFragmentViewModel.kt
@@ -6,8 +6,18 @@ import androidx.lifecycle.ViewModel
 
 class MainFragmentViewModel():ViewModel() {
 
+    // LiveData для хранения навигации
     private val _navigationEvent = MutableLiveData<NavigationEvent>()
     var navigationEvent:LiveData<NavigationEvent> = _navigationEvent
+
+    // LiveData для хранения текста поиска
+    private val _searchText = MutableLiveData<String>()
+    var searchText:LiveData<String> = _searchText
+
+    // Функция для обновления текста поиска
+    fun updateSearchText(text:String){
+        _searchText.value = text
+    }
 
     // Функция для обработки нажатия кнопки
     fun onApplyButtonClick(){
