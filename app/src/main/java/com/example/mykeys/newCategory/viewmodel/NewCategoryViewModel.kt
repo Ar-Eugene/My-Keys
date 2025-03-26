5536a41def033cfb02686a494bee853841f0d9d0
User <artemenkoe2711@gmail.com>
2025-03-26 20:03:24 +0400

    создана логика в vm для работы с room

--- app/src/main/java/com/example/mykeys/newCategory/viewmodel/NewCategoryViewModel.kt
+++ app/src/main/java/com/example/mykeys/newCategory/viewmodel/NewCategoryViewModel.kt
@@ -1,12 +1,34 @@
 package com.example.mykeys.newCategory.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mykeys.newCategory.domain.interactor.CategoryInteractor
+import com.example.mykeys.newCategory.domain.models.CategoryModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class NewCategoryViewModel @Inject constructor(
-    interactor: CategoryInteractor)
-    : ViewModel() {
+    private val categoryInteractor: CategoryInteractor
+) : ViewModel() {
+
+    private val _category = MutableStateFlow<List<CategoryModel>>(emptyList())
+    var category: StateFlow<List<CategoryModel>> = _category
+
+    init {
+        viewModelScope.launch {
+            categoryInteractor.getCategory().collect { categories ->
+                _category.value=categories
+            }
+        }
+    }
+
+    fun addCategory(categoryModel: CategoryModel){
+        viewModelScope.launch {
+            categoryInteractor.createCategory(categoryModel)
+        }
+    }
 }
