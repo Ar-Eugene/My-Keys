42d87669a72ec75c2a92f5f36e32470cfc4ebbc3
User <artemenkoe2711@gmail.com>
2025-05-10 14:47:08 +0400

    доработана валидация

--- app/src/main/java/com/example/mykeys/password/CreatePasswordFragment.kt
+++ app/src/main/java/com/example/mykeys/password/CreatePasswordFragment.kt
@@ -1,7 +1,6 @@
 package com.example.mykeys.password
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -34,52 +33,98 @@ class CreatePasswordFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Добавляем слушатель потери фокуса у edtPassword
+        binding.edtPassword.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                validatePasswordLength()
+            }
+        }
+
+        // Добавляем слушатель потери фокуса у edtConfirmPassword
+        binding.edtConfirmPassword.setOnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                validConfirmPassword()
+            }
+        }
+
         binding.btnCreatePassword.setOnClickListener {
             val password = binding.edtPassword.text.toString()
             val confirmPassword = binding.edtConfirmPassword.text.toString()
             val keyWord = binding.edtConfirmKeyword.text.toString()
 
-            var isValid = true
-
-            // Проверка длины пароля
-            if (password.isEmpty() || password.length < 5) {
-                binding.edtPassword.error = getString(R.string.error_password_too_short)
-                isValid = false
-            } else {
-                binding.edtPassword.error = null
+            if (!validateInputs(password, confirmPassword, keyWord)) {
+                return@setOnClickListener
             }
 
-            // Проверка повторного пароля
-            if (confirmPassword.isEmpty()) {
-                binding.edtConfirmPassword.error = "Повторите пароль"
-                isValid = false
-            } else if (password != confirmPassword) {
-                binding.edtConfirmPassword.error = "Пароли не совпадают"
-                isValid = false
-            } else {
-                binding.edtConfirmPassword.error = null
-            }
-
-            // Проверка ключевого слова
-            if (keyWord.isEmpty()) {
-                binding.edtConfirmKeyword.error = "Укажите ключевое слово"
-                isValid = false
-            } else {
-                binding.edtConfirmKeyword.error = null
-            }
-
-            if (!isValid) return@setOnClickListener
-
-            // Сохраняем данные
             passwordManager.savePassword(password)
             passwordManager.saveKeyWord(keyWord)
-            Log.d("PasswordManager", "Saved keyword: $keyWord")
 
             // Навигация
             findNavController().navigate(R.id.action_createPasswordFragment_to_enterPasswordFragment)
         }
 
     }
+
+    private fun validateInputs(
+        password: String,
+        confirmPassword: String,
+        keyWord: String
+    ): Boolean {
+        if (password.isEmpty() || confirmPassword.isEmpty() || keyWord.isEmpty()) {
+            Toast.makeText(
+                requireContext(),
+                "Пожалуйста, заполните все поля",
+                Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
+
+        // Проверка на совпадение паролей
+        if (password != confirmPassword) {
+            binding.edtConfirmPassword.error = getString(R.string.password_not_confirm)
+            return false
+        } else {
+            binding.edtConfirmPassword.error = null
+        }
+
+        // Проверка ключевого слова
+        if (keyWord.isEmpty()) {
+            binding.edtConfirmKeyword.error = "Укажите ключевое слово"
+        } else {
+            binding.edtConfirmKeyword.error = null
+        }
+
+        return validatePasswordLength()
+    }
+
+    // валидация на длину
+    private fun validatePasswordLength(): Boolean {
+        val newPassword = binding.edtPassword.text.toString()
+        var isValid = newPassword.length <= 5
+
+        if (newPassword.isNotEmpty() && !isValid) {
+            binding.edtPassword.error = getString(R.string.error_password_too_short)
+            return false
+        } else {
+            binding.edtPassword.error = null
+        }
+        return isValid
+    }
+
+    // валидация на равенство confirmPassword с password
+    private fun validConfirmPassword(): Boolean {
+        val password = binding.edtPassword.text.toString()
+        val confirmPassword = binding.edtConfirmPassword.text.toString()
+        var isValid = confirmPassword == password
+        if (confirmPassword != password) {
+            binding.edtConfirmPassword.error = getString(R.string.password_not_confirm)
+            return false
+        } else {
+            binding.edtConfirmPassword.error = null
+        }
+        return isValid
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
