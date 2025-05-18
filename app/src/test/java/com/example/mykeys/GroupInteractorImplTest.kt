f484b046ae352d134d0e322573295a460399b580
User <artemenkoe2711@gmail.com>
2025-05-18 12:02:37 +0400

    поправил первый тест, работаем с моковыми моделями. Полностью покрыл тестами GroupInteractorImpl

--- app/src/test/java/com/example/mykeys/GroupInteractorImplTest.kt
+++ app/src/test/java/com/example/mykeys/GroupInteractorImplTest.kt
@@ -4,7 +4,11 @@ import com.example.mykeys.newGroup.domain.interactor.impl.GroupInteractorImpl
 import com.example.mykeys.newGroup.domain.models.GroupModel
 import com.example.mykeys.newGroup.domain.repository.GroupRepository
 import com.google.common.truth.Truth.assertThat
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.flow.flowOf
@@ -22,34 +26,94 @@ class GroupInteractorImplTest {
     }
 
     @Test
-    fun `getGroup should return flow from repository`() = runTest {
+    fun `getGroup - should return flow from repository`() = runTest {
         // создаем список для теста
-        val testFlow = listOf(
-            GroupModel(
-                id = 1,
-                nameGroup = "nameGroup",
-                emailGroup = "emailGroup",
-                passwordGroup = "passwordGroup",
-                loginGroup = "loginGroup",
-                position = 1,
-            ), GroupModel(
-                id = 2,
-                nameGroup = "nameGroup2",
-                emailGroup = "emailGroup2",
-                passwordGroup = "passwordGroup2",
-                loginGroup = "loginGroup2",
-                position = 2,
-            )
-        )
+        val testFlow = flowOf(listOf(mockk<GroupModel>(), mockk<GroupModel>()))
         // создаем мок
-        every { repository.getGroup() } returns flowOf(testFlow)
+        every { repository.getGroup() } returns testFlow
 
         val resultTest = interactor.getGroup()
+
         // проверяем, что Flow содержит ожидаемый результат
-        resultTest.collect { group ->
-            assertThat(group).isEqualTo(testFlow)
-        }
+        assertThat(resultTest).isEqualTo(testFlow)
+
         // проверяем, что реп вызывается
-        verify { repository.getGroup() }
+        verify(exactly = 1) { repository.getGroup() }
+    }
+
+    @Test
+    fun `createGroup - check that the repository is created by the group`() = runTest {
+        // grate test object
+        val group = mockk<GroupModel>()
+        /** mockk is the suspend create through coEvery
+         * tell that any argument GroupModel correspond this rule
+         * "just Runs" tell that the method should simply return without returning a value
+         * createGroup this suspend method and return type Unit
+         */
+        coEvery { repository.createGroup(any()) } just Runs
+
+        interactor.createGroup(group)
+
+        //coVerify in suspend method analogue verify
+        coVerify(exactly = 1) { repository.createGroup(group) }
+    }
+
+    @Test
+    fun `deleteGroupById - check that the repository is delete group by ID`() = runTest {
+        // create test data
+        val testId = 1
+
+        // create mockk for suspend method
+        coEvery { repository.deleteGroupById(any()) } just Runs
+
+        // call test method
+        interactor.deleteGroupById(testId)
+
+        coVerify(exactly = 1) { repository.deleteGroupById(testId) }
+    }
+
+    @Test
+    fun `updateGroup - check that the repository update group`() = runTest {
+        val group = mockk<GroupModel>()
+
+        coEvery { repository.updateGroup(any()) } just Runs
+
+        interactor.updateGroup(group)
+
+        coVerify(exactly = 1) { repository.updateGroup(group) }
+    }
+
+    @Test
+    fun `isGroupNameExists - check that the group exists with nameGroup`() = runTest {
+
+        val testName = "Test"
+        val testId = 1
+
+        coEvery { repository.isGroupNameExists(testName, testId) } returns true
+
+        val result = interactor.isGroupNameExists(testName, testId)
+
+        assertThat(result).isTrue()
+
+        coVerify(exactly = 1) { repository.isGroupNameExists(testName, testId) }
+    }
+
+    @Test
+    fun `isGroupNameExists - check that the group does not exist with nameGroup`() = runTest {
+        // Подготавливаем тестовые данные
+        val testName = "Test"
+        val testId = 1
+
+        // Настраиваем мок для suspend метода
+        coEvery { repository.isGroupNameExists(testName, testId) } returns false
+
+        // Вызываем тестируемый метод
+        val result = interactor.isGroupNameExists(testName, testId)
+
+        // Проверяем результат
+        assertThat(result).isFalse()
+
+        // Проверяем, что метод репозитория был вызван с правильными параметрами
+        coVerify(exactly = 1) { repository.isGroupNameExists(testName, testId) }
     }
 }
